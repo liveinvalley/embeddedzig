ccfdc35f8b8e84f08c05298db9a77a2de0b49c64
liveinvalley <156389201+liveinvalley@users.noreply.github.com>
2024-11-13 20:57:44 +0900

    feat: initial firmware build.

--- build.zig
+++ build.zig
@@ -8,26 +8,18 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    // const target = b.standardTargetOptions(.{});
+    const target = b.resolveTargetQuery(.{ .os_tag = .freestanding, .cpu_arch = .thumb, .abi = .eabihf, .ofmt = .elf });
 
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "embeddedzig",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
-    b.installArtifact(lib);
+    //b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "embeddedzig",
@@ -35,6 +27,8 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe.setLinkerScript(b.path("src/memory.ld"));
+    //    exe.addAssemblyFile(b.path("src/startup.s"));
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,74 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+extern const _data_loadaddr: u32;
+extern var _data: u32;
+extern const _edata: u32;
+extern var _bss: u32;
+extern const _ebss: u32;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+export fn resetHandler() void {
+    // Copy data from flash to RAM
+    const data_loadaddr: [*]const u8 = @ptrCast(&_data_loadaddr);
+    const data = @as([*]u8, @ptrCast(&_data));
+    const data_size = @intFromPtr(&_edata) - @intFromPtr(&_data);
+    for (data_loadaddr[0..data_size], 0..) |d, i| data[i] = d;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // Clear the bss
+    const bss: [*]u8 = @ptrCast(&_bss);
+    const bss_size = @intFromPtr(&_ebss) - @intFromPtr(&_bss);
+    for (bss[0..bss_size]) |*b| b.* = 0;
 
-    try bw.flush(); // don't forget to flush!
+    main();
+    unreachable;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn test2() void {
+    main();
 }
+
+pub fn main() void {
+    test2();
+}
+
+// These two are the default empty implementations for exception handlers
+export fn blockingHandler() void {
+    while (true) {}
+}
+
+export fn nullHandler() void {}
+
+// This comes from the linker script and represents the initial stack pointer address.
+// Not a function, but pretend it is to suppress type error
+extern fn _stack() void;
+
+// These are the exception handlers, which are weakly linked to the default handlers
+// in the linker script
+//extern fn resetHandler() void;
+extern fn nmiHandler() void;
+extern fn hardFaultHandler() void;
+extern fn memoryManagementFaultHandler() void;
+extern fn busFaultHandler() void;
+extern fn usageFaultHandler() void;
+extern fn svCallHandler() void;
+extern fn debugMonitorHandler() void;
+extern fn pendSVHandler() void;
+extern fn sysTickHandler() void;
+
+export const vector_table linksection(".vectors") = [_]?*const @TypeOf(resetHandler){
+    &_stack,
+    &resetHandler, // Reset
+    &nmiHandler, // NMI
+    &hardFaultHandler, // Hard fault
+    &memoryManagementFaultHandler, // Memory management fault
+    &busFaultHandler, // Bus fault
+    &usageFaultHandler, // Usage fault
+    null, // Reserved 1
+    null, // Reserved 2
+    null, // Reserved 3
+    null, // Reserved 4
+    &svCallHandler, // SVCall
+    &debugMonitorHandler, // Debug monitor
+    null, // Reserved 5
+    &pendSVHandler, // PendSV
+    &sysTickHandler, // SysTick
+};
